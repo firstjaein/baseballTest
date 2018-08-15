11ac818b659eb6e588fff3698829690bafa30e50
firstjaein <firstcitron@gmail.com>
2018-08-16 00:35:08 +0900

    java scanner validate test

---
+++ src/baseballPractice/testValidateNumber.java
@@ -0,0 +1,19 @@
+package baseballPractice;
+
+import java.util.Scanner;
+
+public class testValidateNumber {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("0을 제외한 한자리 숫자만 입력하세요. 문자를 입력하면 에러를 뿜어요.");
+		while (!scan.hasNextInt()) {
+			scan.next();
+			System.err.print("에러! 숫자가 아닙니다. \n재 선택 : ");
+		} // End of while
+		scan.nextLine();
+		int scanNumber = scan.nextInt();
+		System.out.println("입력한 숫자는: " + scanNumber);
+	}
+}

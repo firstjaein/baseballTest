7cf0c7dd4e5d8b35c40738d622f7f0109f48059d
firstjaein <firstcitron@gmail.com>
2018-08-16 23:50:20 +0900

    ENDcount 중복 코드 제거

--- src/baseballPractice/RandomNumber.java
+++ src/baseballPractice/RandomNumber.java
@@ -4,13 +4,13 @@ import java.util.Random;
 //중복없는 난수 생성
 public class RandomNumber {
 
-	final int ENDcount = 3;
-	int comDigit[] = new int[ENDcount];
+//	final int ENDcount = 3;
+	int comDigit[] = new int[BaseballMain.EndCount];
 
 	public void makeRandomNumber() {
 		Random random = new Random();
 
-		for (int i = 0; i < ENDcount; i++) {
+		for (int i = 0; i < BaseballMain.EndCount; i++) {
 			comDigit[i] = random.nextInt(9) + 1;
 			for (int j = 0; j < i; j++) {
 				if (comDigit[i] == comDigit[j]) {
@@ -18,7 +18,7 @@ public class RandomNumber {
 				}
 			}
 		}
-		for (int k = 0; k < ENDcount; k++) {
+		for (int k = 0; k < BaseballMain.EndCount; k++) {
 			System.out.print(comDigit[k] + " ");
 		}
 	} // End of makeRandomNumber

32216bf47afe5d428d9a31aa3595631c75937aa0
firstjaein <firstcitron@gmail.com>
2018-08-16 23:44:48 +0900

    ball count, strike count errors solved!

--- src/baseballPractice/CompareNum.java
+++ src/baseballPractice/CompareNum.java
@@ -1,23 +1,54 @@
 package baseballPractice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CompareNum {
 	public boolean compareNum(int[] fNum, int[] sNum) {
-		final int ENDcount = 3;
-		int fNumCnt = fNum.length;	//user's digit
-		int sNumCnt = sNum.length;	//com's random digit
+//		final int ENDcount = 3;
+		int fNumCnt = fNum.length; // user's digit
+		int sNumCnt = sNum.length; // com's random digit
 		int ballCnt = 0; // ball count
 		int stCnt = 0; // strike count
-		for (int i = 0; i < fNumCnt; i++) {
-			for (int j = 0; j < sNumCnt; j++) {
-				if (fNum[i] == sNum[j] && i == j) {
+		
+		List<Integer> alist = new ArrayList<Integer>();
+		for(int i : fNum)
+		{
+			alist.add(i);
+		}
+		int idx = sNum.length;
+		for(int i =0; i <idx; i++)
+		{
+			if(alist.contains(sNum[i]))
+			{
+				if(alist.get(i) == sNum[i])
+				{
+					//스트라이크
 					stCnt++;
-				}else{
+				}
+				else
+				{
+					//볼
 					ballCnt++;
 				}
 			}
 		}
+		
+		
+		
+		
+//		for (int i = 0; i < fNumCnt; i++) {
+//			for (int j = 0; j < sNumCnt; j++) {
+//				if (fNum[i] == sNum[j])
+//					if (i == j) {
+//						stCnt++;
+//					} else {
+//						ballCnt++;
+//					}
+//			}
+//		}
 
-		if (stCnt == ENDcount) {
+		if (stCnt == BaseballMain.EndCount) {
 			System.out.println("정답!컴퓨터의 입력값은: " + sNum[0] + "" + sNum[1] + "" + sNum[2]);
 			return true;
 		} else {

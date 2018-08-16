a44bd14e100e48c869a6c2eb12cf9df6b9e85500
firstjaein <firstcitron@gmail.com>
2018-08-16 19:49:48 +0900

    java scanner 정수 입력 예외 처리

--- src/baseballPractice/testValidateNumberTwo.java
+++ src/baseballPractice/testValidateNumberTwo.java
@@ -10,12 +10,16 @@ public class testValidateNumberTwo {
 		System.out.println("정수를 입력하세요.");
 		Scanner input = new Scanner(System.in);
 		int number = 0;
-		
-		try{
-			number = input.nextInt();
-		}catch(InputMismatchException ime){
-			System.out.println("잘못된 입력입니다.정수만 입력하세요.");
+
+		while (true) {
+			try {
+				number = input.nextInt();
+				break;
+			} catch (InputMismatchException ime) {
+				input = new Scanner(System.in);
+				System.out.println("잘못된 입력입니다.정수만 입력하세요.");
+			} catch (Exception e) {
+			}
 		}
-		System.out.println("입력한 정수는: "+ number +"입니다.");
 	}
 }

f4e26d929079378d9ab9d0d9bc17ffe6292b1498
firstjaein <firstcitron@gmail.com>
2018-08-17 20:42:36 +0900

    while문 수정

--- src/baseballPractice/BaseballMain.java
+++ src/baseballPractice/BaseballMain.java
@@ -5,29 +5,31 @@ import java.util.Scanner;
 public class BaseballMain {
 
 	final static int EndCount = 3;
-	
+
 	public static void main(String[] args) {
-		// boolean isFinish = false;
 		RandomNumber randomNumber = new RandomNumber();
 		int[] userDigit = new int[EndCount];
 		System.out.println("3자리 랜덤숫자를 맞춰보세요.");
 		Scanner sc = new Scanner(System.in);
 		randomNumber.makeRandomNumber();
 
+		while (true) {
 			for (int i = 0; i < userDigit.length; i++) {
 				System.out.println("");
 				System.out.println(i + " 숫자입력:");
-				try
-				{
+				try {
 					userDigit[i] = Integer.parseInt(sc.next());
-				}
-				catch(NumberFormatException nfe)
-				{
-					System.err.println("잘못된 입력");
+				} catch (NumberFormatException nfe) {
+					System.err.println("잘못된 입력.숫자를 입력하세요.");
 					i--;
 				}
 			}
 			CompareNum compareNum = new CompareNum();
-			compareNum.compareNum(userDigit, randomNumber.getRandomNumber());
-		} //End of While
-	} // End of Main
+			boolean isFinish = false;
+			isFinish = compareNum.compareNum(userDigit, randomNumber.getRandomNumber());
+			if (isFinish == true) {
+				randomNumber.makeRandomNumber();
+			}
+		} // End of while
+	} // ENd of main
+}

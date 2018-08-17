23922bc429a49df13649cc21784835026ba43250
firstjaein <firstcitron@gmail.com>
2018-08-17 20:26:58 +0900

    문자입력시 예외발생 수정.

--- src/baseballPractice/BaseballMain.java
+++ src/baseballPractice/BaseballMain.java
@@ -7,13 +7,9 @@ public class BaseballMain {
 	final static int EndCount = 3;
 	
 	public static void main(String[] args) {
-//		final int ENDcount = 3;
 		// boolean isFinish = false;
 		RandomNumber randomNumber = new RandomNumber();
 		int[] userDigit = new int[EndCount];
-		// System.out.println("컴퓨터의 랜덤숫자는..");
-		// randomNumber.makeRandomNumber();
-		System.out.println("");
 		System.out.println("3자리 랜덤숫자를 맞춰보세요.");
 		Scanner sc = new Scanner(System.in);
 		randomNumber.makeRandomNumber();
@@ -23,20 +19,15 @@ public class BaseballMain {
 				System.out.println(i + " 숫자입력:");
 				try
 				{
-					userDigit[i] = Integer.parseInt(sc.next()); // 숫자만 입력받게 예외코드 작성 필요.
+					userDigit[i] = Integer.parseInt(sc.next());
 				}
 				catch(NumberFormatException nfe)
 				{
-					System.out.println("잘못된 입력");
+					System.err.println("잘못된 입력");
 					i--;
 				}
-				//System.out.println();
-			
-			//System.out.println("새로운 게임을 시작");
 			}
 			CompareNum compareNum = new CompareNum();
-			// isFinish = compareNum.compareNum(userDigit,
-			// randomNumber.getRandomNumber());
 			compareNum.compareNum(userDigit, randomNumber.getRandomNumber());
-		} //End of While4
+		} //End of While
 	} // End of Main

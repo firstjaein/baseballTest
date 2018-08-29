caaf60fa197572bbe978f7fb76ad9003f51ac6a1
firstjaein <firstcitron@gmail.com>
2018-08-29 20:20:23 +0900

    최종수정

--- src/baseballPractice/RandomNumber.java
+++ src/baseballPractice/RandomNumber.java
@@ -10,18 +10,18 @@ public class RandomNumber {
 		Random random = new Random();
 		for (int i = 0; i < BaseballMain.EndCount; i++) {
 			comDigit[i] = random.nextInt(9) + 1;
-			for (int j = 0; j < i; j++) {
+			for (int j = 0; j < i; j++) {	//중복 숫자 생성 방지
 				if (comDigit[i] == comDigit[j]) {
 					i--;
 				}
 			}
 		} // End of for
-		for (int k = 0; k < BaseballMain.EndCount; k++) {
+		for (int k = 0; k < BaseballMain.EndCount; k++) {	//중복없는 랜덤숫자 배열 생성
 			System.out.print(comDigit[k] + " ");
 		}
 	} // End of makeRandomNumber
 
 	public int[] getRandomNumber() {
-		return this.comDigit;
+		return this.comDigit;	//메인에서 중복없는 랜덤숫자 배열 호출
 	}
 }

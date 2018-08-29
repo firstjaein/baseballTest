056637614c218cb1ebf6bfe7c03845e7dbb3988d
firstjaein <firstcitron@gmail.com>
2018-08-29 15:15:03 +0900

    한글 주석 에러 수정

--- src/baseballPractice/RandomNumber.java
+++ src/baseballPractice/RandomNumber.java
@@ -1,15 +1,13 @@
 package baseballPractice;
+
 import java.util.Random;
 
-//중복없는 난수 생성
 public class RandomNumber {
 
-//	final int ENDcount = 3;
 	int comDigit[] = new int[BaseballMain.EndCount];
 
 	public void makeRandomNumber() {
 		Random random = new Random();
-
 		for (int i = 0; i < BaseballMain.EndCount; i++) {
 			comDigit[i] = random.nextInt(9) + 1;
 			for (int j = 0; j < i; j++) {
@@ -17,7 +15,7 @@ public class RandomNumber {
 					i--;
 				}
 			}
-		}
+		} // End of for
 		for (int k = 0; k < BaseballMain.EndCount; k++) {
 			System.out.print(comDigit[k] + " ");
 		}

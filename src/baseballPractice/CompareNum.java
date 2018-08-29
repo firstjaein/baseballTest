d7829f98d927e2dee7405f18a15701c7fca4a766
firstjaein <firstcitron@gmail.com>
2018-08-29 15:14:22 +0900

    불필요한 주석 삭제 및 한글 주석 에러 수정

--- src/baseballPractice/CompareNum.java
+++ src/baseballPractice/CompareNum.java
@@ -5,51 +5,28 @@ import java.util.List;
 
 public class CompareNum {
 	public boolean compareNum(int[] fNum, int[] sNum) {
-//		final int ENDcount = 3;
 		int fNumCnt = fNum.length; // user's digit
 		int sNumCnt = sNum.length; // com's random digit
 		int ballCnt = 0; // ball count
 		int stCnt = 0; // strike count
-		
+
 		List<Integer> alist = new ArrayList<Integer>();
-		for(int i : fNum)
-		{
+		for (int i : fNum) {
 			alist.add(i);
 		}
 		int idx = sNum.length;
-		for(int i =0; i <idx; i++)
-		{
-			if(alist.contains(sNum[i]))
-			{
-				if(alist.get(i) == sNum[i])
-				{
-					//½ºÆ®¶óÀÌÅ©
+		for (int i = 0; i < idx; i++) {
+			if (alist.contains(sNum[i])) {
+				if (alist.get(i) == sNum[i]) {
 					stCnt++;
-				}
-				else
-				{
-					//º¼
+				} else {
 					ballCnt++;
 				}
 			}
 		}
-		
-		
-		
-		
-//		for (int i = 0; i < fNumCnt; i++) {
-//			for (int j = 0; j < sNumCnt; j++) {
-//				if (fNum[i] == sNum[j])
-//					if (i == j) {
-//						stCnt++;
-//					} else {
-//						ballCnt++;
-//					}
-//			}
-//		}
 
 		if (stCnt == BaseballMain.EndCount) {
-			System.out.println("Á¤´ä!ÄÄÇ»ÅÍÀÇ ÀÔ·Â°ªÀº: " + sNum[0] + "" + sNum[1] + "" + sNum[2]);
+			System.out.println("ì •ë‹µ!ì»´í“¨í„°ì˜ ìž…ë ¥ê°’ì€ " + sNum[0] + "" + sNum[1] + "" + sNum[2]);
 			return true;
 		} else {
 			System.out.println((ballCnt + stCnt) == 0 ? "nothing" : (ballCnt + " ball," + stCnt + " strike."));
